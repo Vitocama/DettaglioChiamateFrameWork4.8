52f9ea72baf385264beea45853b5645ddf71af1e
vito <camarretta@chiocciola>
2024-05-27 22:25:40 +0200

    docEntrate

--- DettaglioChiamate/DettaglioChiamate/frmDocEntrate.Designer.cs
+++ DettaglioChiamate/DettaglioChiamate/frmDocEntrate.Designer.cs
@@ -31,8 +31,8 @@
             this.lblAnno = new System.Windows.Forms.Label();
             this.numericUpDown = new System.Windows.Forms.NumericUpDown();
             this.bpbBox = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.bntPath = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.bntCrea = new System.Windows.Forms.Button();
             this.bntEsci = new System.Windows.Forms.Button();
             this.bntLog = new System.Windows.Forms.Button();
@@ -48,110 +48,126 @@
             // 
             this.lblAnno.AutoSize = true;
             this.lblAnno.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAnno.Location = new System.Drawing.Point(22, 26);
+            this.lblAnno.Location = new System.Drawing.Point(29, 32);
+            this.lblAnno.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblAnno.Name = "lblAnno";
-            this.lblAnno.Size = new System.Drawing.Size(126, 16);
+            this.lblAnno.Size = new System.Drawing.Size(159, 20);
             this.lblAnno.TabIndex = 0;
             this.lblAnno.Text = "SELEZIONE ANNO:";
             // 
             // numericUpDown
             // 
             this.numericUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numericUpDown.Location = new System.Drawing.Point(189, 21);
+            this.numericUpDown.Location = new System.Drawing.Point(252, 26);
+            this.numericUpDown.Margin = new System.Windows.Forms.Padding(4);
             this.numericUpDown.Name = "numericUpDown";
-            this.numericUpDown.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown.Size = new System.Drawing.Size(160, 26);
             this.numericUpDown.TabIndex = 1;
+            this.numericUpDown.ValueChanged += new System.EventHandler(this.numericUpDown_ValueChanged);
             // 
             // bpbBox
             // 
             this.bpbBox.Controls.Add(this.bntPath);
             this.bpbBox.Controls.Add(this.textBox1);
-            this.bpbBox.Location = new System.Drawing.Point(578, 21);
+            this.bpbBox.Location = new System.Drawing.Point(771, 26);
+            this.bpbBox.Margin = new System.Windows.Forms.Padding(4);
             this.bpbBox.Name = "bpbBox";
-            this.bpbBox.Size = new System.Drawing.Size(318, 79);
+            this.bpbBox.Padding = new System.Windows.Forms.Padding(4);
+            this.bpbBox.Size = new System.Drawing.Size(424, 97);
             this.bpbBox.TabIndex = 3;
             this.bpbBox.TabStop = false;
             this.bpbBox.Text = "SALVATAGGIO FILE";
             this.bpbBox.Enter += new System.EventHandler(this.bpbBox_Enter);
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(29, 29);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
-            // 
             // bntPath
             // 
-            this.bntPath.Location = new System.Drawing.Point(208, 25);
+            this.bntPath.Location = new System.Drawing.Point(277, 31);
+            this.bntPath.Margin = new System.Windows.Forms.Padding(4);
             this.bntPath.Name = "bntPath";
-            this.bntPath.Size = new System.Drawing.Size(75, 23);
+            this.bntPath.Size = new System.Drawing.Size(100, 28);
             this.bntPath.TabIndex = 1;
             this.bntPath.Text = " ...";
             this.bntPath.UseVisualStyleBackColor = true;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 36);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 0;
+            // 
             // bntCrea
             // 
-            this.bntCrea.Location = new System.Drawing.Point(946, 37);
+            this.bntCrea.Location = new System.Drawing.Point(1261, 46);
+            this.bntCrea.Margin = new System.Windows.Forms.Padding(4);
             this.bntCrea.Name = "bntCrea";
-            this.bntCrea.Size = new System.Drawing.Size(84, 50);
+            this.bntCrea.Size = new System.Drawing.Size(112, 62);
             this.bntCrea.TabIndex = 4;
             this.bntCrea.Text = "CREA";
             this.bntCrea.UseVisualStyleBackColor = true;
             // 
             // bntEsci
             // 
-            this.bntEsci.Location = new System.Drawing.Point(946, 477);
+            this.bntEsci.Location = new System.Drawing.Point(1261, 587);
+            this.bntEsci.Margin = new System.Windows.Forms.Padding(4);
             this.bntEsci.Name = "bntEsci";
-            this.bntEsci.Size = new System.Drawing.Size(84, 50);
+            this.bntEsci.Size = new System.Drawing.Size(112, 62);
             this.bntEsci.TabIndex = 5;
             this.bntEsci.Text = "ESCI";
             this.bntEsci.UseVisualStyleBackColor = true;
             // 
             // bntLog
             // 
-            this.bntLog.Location = new System.Drawing.Point(281, 461);
+            this.bntLog.Location = new System.Drawing.Point(375, 567);
+            this.bntLog.Margin = new System.Windows.Forms.Padding(4);
             this.bntLog.Name = "bntLog";
-            this.bntLog.Size = new System.Drawing.Size(84, 50);
+            this.bntLog.Size = new System.Drawing.Size(112, 62);
             this.bntLog.TabIndex = 6;
             this.bntLog.Text = "LOG";
             this.bntLog.UseVisualStyleBackColor = true;
+            this.bntLog.Click += new System.EventHandler(this.bntLog_Click_1);
             // 
             // bntElabora
             // 
-            this.bntElabora.Location = new System.Drawing.Point(138, 461);
+            this.bntElabora.Location = new System.Drawing.Point(184, 567);
+            this.bntElabora.Margin = new System.Windows.Forms.Padding(4);
             this.bntElabora.Name = "bntElabora";
-            this.bntElabora.Size = new System.Drawing.Size(105, 50);
+            this.bntElabora.Size = new System.Drawing.Size(140, 62);
             this.bntElabora.TabIndex = 7;
             this.bntElabora.Text = "ELEBORAZIONE";
             this.bntElabora.UseVisualStyleBackColor = true;
-            this.bntElabora.Click += new System.EventHandler(this.button5_Click);
+            this.bntElabora.Click += new System.EventHandler(this.bntElabora_Click_1);
             // 
             // bntEstrai
             // 
-            this.bntEstrai.Location = new System.Drawing.Point(25, 461);
+            this.bntEstrai.Location = new System.Drawing.Point(33, 567);
+            this.bntEstrai.Margin = new System.Windows.Forms.Padding(4);
             this.bntEstrai.Name = "bntEstrai";
-            this.bntEstrai.Size = new System.Drawing.Size(84, 50);
+            this.bntEstrai.Size = new System.Drawing.Size(112, 62);
             this.bntEstrai.TabIndex = 8;
             this.bntEstrai.Text = "ESTRAI";
             this.bntEstrai.UseVisualStyleBackColor = true;
+            this.bntEstrai.Click += new System.EventHandler(this.bntEstrai_Click);
             // 
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(-132, 124);
+            this.dataGridView1.Location = new System.Drawing.Point(-176, 153);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(1162, 318);
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(1549, 391);
             this.dataGridView1.TabIndex = 9;
             // 
             // FormfrmDocEntrate
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1108, 562);
+            this.ClientSize = new System.Drawing.Size(1477, 692);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.bntEstrai);
             this.Controls.Add(this.bntElabora);
@@ -161,6 +177,7 @@
             this.Controls.Add(this.bpbBox);
             this.Controls.Add(this.numericUpDown);
             this.Controls.Add(this.lblAnno);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FormfrmDocEntrate";
             this.Text = "FileDocumentoEntrate";
             this.Load += new System.EventHandler(this.Form1_Load);
--- DettaglioChiamate/DettaglioChiamate/frmDocEntrate.cs
+++ DettaglioChiamate/DettaglioChiamate/frmDocEntrate.cs
@@ -8,11 +8,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data;
+using System.IO;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
 
 namespace DettagliChiamate
 {
     public partial class FormfrmDocEntrate : Form
     {
+
+        string connection = "Server=LAPTOP-8OH69FI3\\SQLEXPRESS01;Database=kongnew;Trusted_Connection=True;Encrypt=false";
+        SQLControl tblufficio = new SQLControl();
+        DataTable dt = new DataTable();
+       
+
         public FormfrmDocEntrate()
         {
             InitializeComponent();
@@ -20,6 +30,15 @@ namespace DettagliChiamate
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            numericUpDown.Maximum = DateTime.Now.Year - 1;
+            numericUpDown.Value = DateTime.Now.Year - 1;
+            numericUpDown.Minimum = 2015;
+
+            bpbBox.Visible = false;
+            bntElabora.Visible = false;
+            bntLog.Visible = false;
+            bntCrea.Visible = false;
+            File.Delete(pathDocument());
 
         }
 
@@ -28,9 +47,380 @@ namespace DettagliChiamate
 
         }
 
-        private void button5_Click(object sender, EventArgs e)
+       
+
+        private void numericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+            dt.Clear();
+
+            bpbBox.Visible = false;
+            bntLog.Visible = false;
+        }
+
+        private void bntEstrai_Click(object sender, EventArgs e)
+        {
+            bntCrea.Visible = false;
+            bntLog.Visible = false;
+            bntElabora.Visible = false;
+            bntLog.Visible = false;
+            bpbBox.Visible = false;
+            estrai();
+        }
+
+
+        private bool estrai()
+        {
+            Cursor = Cursors.WaitCursor;
+            //creazione di un dataTable e visibile in griglia
+            DataTable dt = new DataTable();
+            tblufficio = new SQLControl(connection);
+            tblufficio.AddParam("@anno", numericUpDown.Value);
+            tblufficio.ExecQuery("select * from voipdettaglio");
+
+
+            if (tblufficio.HasException(true))
+            {
+                MessageBox.Show("La tabella Dettagli ha generato un errore");
+                Cursor = Cursors.Default;
+                return true;
+            }
+
+            dt = tblufficio.DBDT;
+
+            if (dt.Rows.Count == 0)
+            {
+                Cursor = Cursors.Default;
+                MessageBox.Show("La tabella dettagli non sono presenti dati");
+                return true;
+
+            }
+
+            dataGridView1.DataSource = dt;
+            bntElabora.Visible = true;
+            Cursor = Cursors.Default;
+            return true;
+        }
+
+        private void bntElabora_Click(object sender, EventArgs e)
+        {
+            bntCrea.Visible = false;
+            bntLog.Visible = false;
+            bpbBox.Visible = false;
+            elabora("sp_creazioneTabellaUfficio", "ufficio");
+            creaFileLog();
+
+        }
+
+
+        private bool elabora(string procedure, string tabella)
+        {
+            Cursor = Cursors.WaitCursor;
+            dt.Clear();
+            tblufficio = new SQLControl(connection);
+            tblufficio.AddParam("@anno", numericUpDown.Value);
+            tblufficio.ExecStoredProcedure("sp_creazioneTabellaUfficio");
+
+
+
+            if (tblufficio.HasException(true))
+            {
+                Cursor = Cursors.Default;
+                MessageBox.Show("errore nella procedura");
+                return true;
+
+            }
+
+
+
+
+
+            tblufficio = new SQLControl(connection);
+            tblufficio.ExecQuery("select * from " + tabella);
+
+            if (tblufficio.HasException(true))
+            {
+                MessageBox.Show("Errore di connessione alla tabella " + tabella);
+                Cursor = Cursors.Default;
+            }
+
+            dt = tblufficio.DBDT;
+
+            if (dt.Rows.Count == 0)
+            {
+                Cursor = Cursors.Default;
+                MessageBox.Show("la tabella non presenta dati al suo interno");
+                return false;
+
+            }
+
+
+
+
+            Cursor = Cursors.Default;
+
+            return true;
+        }
+
+
+        private void creaFileLog()
+        {
+            Cursor = Cursors.WaitCursor;
+            string path = pathDocument();
+
+            int rowCount = 0; // Contatore per le righe
+
+            try
+            {
+                using (SqlConnection cnn = new SqlConnection(connection))
+                {
+                    cnn.Open();
+
+                    // Esecuzione della stored procedure
+                    using (SqlCommand command = new SqlCommand("sp_filediLog", cnn))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        // Utilizzo SqlDataReader per leggere i dati restituiti dalla stored procedure
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            // StreamWriter per scrivere i risultati in un file
+                            using (StreamWriter writer = new StreamWriter(path, false)) // Imposta append a false per sovrascrivere il file esistente
+                            {
+                                while (reader.Read())
+                                {
+
+                                    writer.Write("Codice fiscale:");
+                                    writer.Write(reader.GetValue(0).ToString());
+                                    string erroreDati = reader.GetValue(9).ToString().Substring(0, reader.GetValue(9).ToString().Length - 1);
+
+                                    writer.Write(" dati da correggere perchè non inseriti correttamente:" + erroreDati);
+
+
+
+                                    writer.WriteLine(); // Passa alla nuova riga dopo ogni record
+                                    rowCount++; // Incrementa il contatore di righe per ogni riga letta
+                                }
+                            }
+                        }
+                    }
+                }
+
+                // Messaggio di conferma sul numero di righe scritte nel file
+                if (rowCount > 0)
+                {
+                    bntLog.Visible = true;
+                    bpbBox.Visible = false;
+                    bntElabora.Visible = false;
+                    MessageBox.Show($"Sono state scritte {rowCount} righe nel file di log.", "Operazione completata", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    Cursor = Cursors.Default;
+
+                }
+                else
+                {
+                    bpbBox.Visible = true;
+                    bntElabora.Visible = false;
+                    bntLog.Visible = false;
+                    Cursor = Cursors.Default;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Gestione delle eccezioni mostrando un messaggio di errore
+                MessageBox.Show("Errore durante l'elaborazione: " + ex.Message, "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Cursor = Cursors.Default;
+            }
+        }
+
+
+        private string pathDocument()
         {
 
+
+
+
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string pathLog = path + @"\log.txt";
+
+            return pathLog;
+
+
+
+        }
+
+        private void bntLog_Click(object sender, EventArgs e)
+        {
+            bntCrea.Visible = false;
+            string path = pathDocument();
+            Cursor = Cursors.WaitCursor;
+            if (File.Exists(path))
+            {
+                try
+                {
+                    Process.Start(new ProcessStartInfo("notepad.exe", path) { UseShellExecute = true });
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Non è possibile aprire il file di log: " + ex.Message, "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                Cursor = Cursors.Default;
+            }
+            else
+            {
+                MessageBox.Show("Il file di log non esiste.", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            Cursor = Cursors.Default;
+        }
+
+
+        private void bntEsci_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+        private void bntPath_Click(object sender, EventArgs e)
+        {
+            bntCrea.Visible = true;
+
+
+
+            SaveFileDialog path = new SaveFileDialog();
+            if (path.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = path.FileName;
+            }
+        }
+
+
+
+        private void bntCrea_Click(object sender, EventArgs e)
+        {
+            DateTime data = DateTime.Now;
+            string dataAll = data.ToString("yyyy-MM-dd");
+            string fileTxt = textBox1.Text + numericUpDown.Value.ToString() + "(" + dataAll + ")" + ".txt";
+            if (string.IsNullOrEmpty(textBox1.Text) == false)
+            {
+                creaFile();
+
+            }
+            else
+            {
+                MessageBox.Show("inserire un path nell'apposito spazio");
+                bntCrea.Visible = false;
+            }
+
+
+        }
+
+
+
+        private void creaFile()
+        {
+            Cursor = Cursors.WaitCursor;
+
+
+            string testa = $"0TEL00220{creaSpazi(17)}01405980531{creaSpazi(5)}ETRURIAWIFI S.R.L.{creaSpazi(42)}CAPALBIO{creaSpazi(32)}GR{creaSpazi(95)}" + numericUpDown.Value + $"{creaSpazi(1554)}A";
+
+
+            string coda = $"9TEL00220{creaSpazi(17)}01405980531{creaSpazi(5)}ETRURIAWIFI S.R.L.{creaSpazi(42)}CAPALBIO{creaSpazi(32)}GR{creaSpazi(95)}" + numericUpDown.Value + $"{creaSpazi(1554)}A";
+
+            DateTime data = DateTime.Now;
+            string dataAll = data.ToString("yyyy-MM-dd");
+            string fileTxt = textBox1.Text + numericUpDown.Value.ToString() + "(" + dataAll + ")" + ".txt";
+
+
+            using (SqlConnection conn = new SqlConnection(connection))
+            {
+                conn.Open();
+                string query = "SELECT * FROM ufficio";
+                SqlCommand command = new SqlCommand(query, conn);
+                SqlDataReader reader = command.ExecuteReader();
+                StreamWriter writer = new StreamWriter(fileTxt);
+                {
+
+                    writer.Write(testa);
+                    writer.WriteLine();
+                    while (reader.Read())
+                    {
+                        int posizione = 1801;
+                        for (int i = 0; i < reader.FieldCount - 1; i++)
+                        {
+
+
+
+                            if (posizione.Equals(i))
+                            {
+                                posizione = 0;
+                                writer.WriteLine();
+                                continue;
+                            }
+
+                            int numcolonna = reader.GetSchemaTable().Rows[i].Field<int>("ColumnSize");
+                            string valoreCampo = reader[i].ToString().ToUpper();
+
+                            writer.Write(valoreCampo.PadRight(numcolonna, ' '));
+
+
+                        }
+
+                        writer.WriteLine();
+                    }
+
+                    writer.Write(coda);
+                    writer.Close();
+                }
+
+                bntCrea.Visible = false;
+
+                Cursor = Cursors.Default;
+
+                MessageBox.Show("Il file creato e' stato salvato correttamente");
+
+            }
+
+
+
+
+        }
+        private string creaSpazi(int lunghezza)
+        {
+            return new string(' ', lunghezza);
+        }
+
+        private void bntElabora_Click_1(object sender, EventArgs e)
+        {
+            bntCrea.Visible = false;
+            bntLog.Visible = false;
+            bpbBox.Visible = false;
+            elabora("sp_creazioneTabellaUfficio", "ufficio");
+            creaFileLog();
+        }
+
+        private void bntLog_Click_1(object sender, EventArgs e)
+        {
+
+            bntCrea.Visible = false;
+            string path = pathDocument();
+            Cursor = Cursors.WaitCursor;
+            if (File.Exists(path))
+            {
+                try
+                {
+                    Process.Start(new ProcessStartInfo("notepad.exe", path) { UseShellExecute = true });
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Non è possibile aprire il file di log: " + ex.Message, "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                Cursor = Cursors.Default;
+            }
+            else
+            {
+                MessageBox.Show("Il file di log non esiste.", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            Cursor = Cursors.Default;
         }
     }
 }
